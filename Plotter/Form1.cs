7af3c87c8b06be681118fb2b064e7f2597cfb30f
DESKTOP-KSEAGUQ\Farook <theelectronicdevil@gmail.com>
2017-09-21 21:28:52 +0300

    moved finding outline and filling sequence methods to the new class

--- Plotter/Form1.cs
+++ Plotter/Form1.cs
@@ -12,7 +12,6 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Plotter;
 
 namespace Plotter
 {
@@ -24,6 +23,9 @@ namespace Plotter
         //Variable Declarations 
         //
         //
+
+        //TODO move all needed variables to the plotter class
+
 
         // add a dictionary that has a name and colour
         public Dictionary<String, Color> Colours = new Dictionary<String, Color>();
@@ -46,11 +48,10 @@ namespace Plotter
 
         bool PlotterAvailable = false;
 
-        int PatternProgressCount;
+        internal int PatternProgressCount;
 
-        List<List<Coordinate>>[] OutlineSequences;
-        List<List<Coordinate>>[] FillingSequences;
-        List<Coordinate> IgnoredPixels;
+        internal List<List<Coordinate>>[] OutlineSequences;
+        internal List<List<Coordinate>>[] FillingSequences;
 
         const string ConfigFileName = "Config.ini";
 
@@ -76,9 +77,6 @@ namespace Plotter
         {
             InitializeComponent();
             pictureBox1.AllowDrop = true;
-            Loader(ConfigFileName);
-            ReloadCols();
-            Handshake();
         }
 
         //
@@ -143,7 +141,9 @@ namespace Plotter
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            Loader(ConfigFileName);
+            ReloadCols();
+            Handshake();
         }
 
         //
@@ -181,7 +181,7 @@ namespace Plotter
         {
             if (ColourMaps != null)
             {
-                PatternMaps = GeneratePatternMaps(ColourMaps);
+                PatternMaps = plotter.GeneratePatternMaps(ColourMaps, this);
             }
             else
                 MessageBox.Show("Colour maps not generated", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
@@ -192,9 +192,9 @@ namespace Plotter
             if (PatternMaps != null)
             {
                 OutlineSequences = new List<List<Coordinate>>[(PatternMaps.Length / 2) - 1];
-                OutlineSequences = GenerateOutlineSequences(PatternMaps);
+                OutlineSequences = plotter.GenerateOutlineSequences(PatternMaps);
                 FillingSequences = new List<List<Coordinate>>[(PatternMaps.Length / 2) - 1];
-                FillingSequences = GenerateFillingSequences(PatternMaps);
+                FillingSequences = plotter.GenerateFillingSequences(PatternMaps);
             }
             else
                 MessageBox.Show("Pattern maps not generated", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
@@ -223,9 +223,11 @@ namespace Plotter
 
         private void openImageToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            OpenFileDialog dlg = new OpenFileDialog();
-            dlg.Title = "Open Image";
-            dlg.Filter = "Plot file (*.plt)|*.plt|bmp files (*.bmp)|*.bmp|jpg files (*.jpg)|*.jpg|png files (*.png)|*.png";
+            OpenFileDialog dlg = new OpenFileDialog
+            {
+                Title = "Open Image",
+                Filter = "Plot file (*.plt)|*.plt|bmp files (*.bmp)|*.bmp|jpg files (*.jpg)|*.jpg|png files (*.png)|*.png"
+            };
             if (dlg.ShowDialog() == DialogResult.OK)
             {
                 pictureBox1.Image = new Bitmap(dlg.FileName);
@@ -464,284 +466,11 @@ namespace Plotter
 
 
 
-        internal Bitmap[,] GeneratePatternMaps(Bitmap[] ColMaps)
-        {
-            //create a 2 dimensional array
-            Bitmap[,] Patterns = new Bitmap[ColMaps.Length-1, 2];
-            //take colour image
-            FormLoadingcs frmldgn = new FormLoadingcs();
-            PatternProgressCount = -1;
-            frmldgn.Show();
-            int counter = 0;
-            for (int i = 0; i < ColMaps.Length - 1; i++)
-            {
-                counter++;
-                Bitmap bmp = ColMaps[i];
-                //create 2 bitmaps, one for the outline and the other for the filling
-                Bitmap Outline = new Bitmap(bmp.Width, bmp.Height, PixelFormat.Format24bppRgb);
-                Bitmap Filling = new Bitmap(bmp.Width, bmp.Height, PixelFormat.Format24bppRgb);
+        
 
-                if (i < compcol.Count - 1)
-                    frmldgn.Text = "Generating Outline - " + Coloursinv[compcol[i]] + " (" + (counter) + "/" + ((compcol.Count - 1) * 2) + ")";
 
-                //call an outlining method on the first bitmap, specify colour
-                Outline = FindOutline(Color.FromArgb(255, 0, 0), bmp, frmldgn);
-                //call a filling method on the second bitmap, specify colour
-                counter++;
-                if (i < compcol.Count - 1)
-                    frmldgn.Text = "Generating Filling - " + Coloursinv[compcol[i]] + " (" + (counter) + "/" + ((compcol.Count - 1) * 2) + ")";
 
-                Filling = FindFilling(Color.FromArgb(0, 255, 0), Outline, bmp, frmldgn);
-                //add the 2 bitmaps to the Array
-                Patterns[i, 0] = Outline;
-                Patterns[i, 1] = Filling;
-            }
-            frmldgn.Close();
-            return Patterns;
-        }
-
-        internal Bitmap FindOutline(Color colour, Bitmap MaptoCheck, FormLoadingcs frmldgn)
-        {
-            //create a list of coordinates to ignore
-            Bitmap linmap = new Bitmap(MaptoCheck.Width, MaptoCheck.Height, PixelFormat.Format24bppRgb);
-            //loop though colour map
-            for (int y = 0; y < MaptoCheck.Height; y++)
-            {
-                PatternProgressCount++;
-                for (int x = 0; x < MaptoCheck.Width; x++)
-                {
-                    Coordinate pixel = new Coordinate(x, y);
-                    //if the pixel is a 1 (black) then check pixel
-                    if (MaptoCheck.GetPixel(x, y) == Color.FromArgb(0, 0, 0))
-                    {
-                        CheckPixel(pixel, colour, MaptoCheck, linmap);
-                    }
-                }
-                frmldgn.setProgress((int)(((float)PatternProgressCount / (float)MaptoCheck.Height * 100f) / (float)((compcol.Count - 1) * 2)));
-            }
-            return linmap;
-        }
-
-        internal void CheckPixel(Coordinate pixel, Color colour, Bitmap MapTC, Bitmap MapTA)
-        {
-            Coordinate[] SurroundersDirect =
-           {
-                new Coordinate(pixel.X()-1,pixel.Y()),
-                new Coordinate(pixel.X()+1,pixel.Y()),
-                new Coordinate(pixel.X(),pixel.Y()-1),
-                new Coordinate(pixel.X(),pixel.Y()+1),
-            };
-            //check the 4 direct pixels surrounding the pixel(up down left right)
-            foreach (Coordinate coord in SurroundersDirect)
-            {
-
-                //if any of them is white then turn the pixel red and keep checking
-                if (coord.X() > 0 && coord.X() < MapTC.Width && coord.Y() > 0 && coord.Y() < MapTC.Height)
-                {
-                    if (MapTC.GetPixel(coord.X(), coord.Y()) == Color.FromArgb(255, 255, 255))
-                    {
-                        MapTA.SetPixel(pixel.X(), pixel.Y(), Color.FromArgb(255, 0, 0));
-                    }
-                }
-            }
-        }
-
-        internal Bitmap FindFilling(Color colour, Bitmap OutlineMap, Bitmap MapToFill, FormLoadingcs frmldgn)
-        {
-            Bitmap FillingMap = new Bitmap(OutlineMap.Width, OutlineMap.Height, PixelFormat.Format24bppRgb);
-            for (int y = 0; y < OutlineMap.Height; y++)
-            {
-                PatternProgressCount++;
-                for (int x = 0; x < OutlineMap.Width; x++)
-                {
-                    if (MapToFill.GetPixel(x, y) == Color.FromArgb(0, 0, 0))
-                    {
-                        if (OutlineMap.GetPixel(x, y) != Color.FromArgb(255, 0, 0))
-                        {
-                            FillingMap.SetPixel(x, y, colour);
-                        }
-                    }
-                }
-                frmldgn.setProgress((int)(((float)PatternProgressCount / (float)MapToFill.Height * 100f) / (float)((compcol.Count - 1) * 2)));
-            }
-            return FillingMap;
-        }
-
-        internal List<List<Coordinate>>[] GenerateOutlineSequences(Bitmap[,] patternMaps)
-        {
-            List<List<Coordinate>>[] OutlineSequenceImageList = new List<List<Coordinate>>[(patternMaps.Length/2)];
-            //loop over colour outlines
-            for (int i = 0; i < ((patternMaps.Length/2)); i++)
-            {   
-                List<List<Coordinate>> SequenceList = new List<List<Coordinate>>();
-                SequenceList = GenerateOutlineSequenceList(patternMaps[i,0]);
-                OutlineSequenceImageList[i] = SequenceList;
-            }
-
-            return OutlineSequenceImageList;
-        }
-
-        internal List<List<Coordinate>> GenerateOutlineSequenceList(Bitmap bitmap)
-        {
-            IgnoredPixels = new List<Coordinate>();
-            int Width = bitmap.Width;
-            int Height = bitmap.Height;
-            List<List<Coordinate>> SL = new List<List<Coordinate>>();
-            for (int j = 0; j<Height; j++)
-            {
-                for (int i = 0; i < Width; i++)
-                {
-                    if (bitmap.GetPixel(i, j) == Color.FromArgb(255, 0, 0))
-                    {
-                        bool Ignored = false;
-                        Coordinate pixel = new Coordinate(i, j);
-                        foreach (Coordinate pixelToCompare in IgnoredPixels)
-                        {
-                            if (pixel.Compare(pixelToCompare))
-                            {
-                                Ignored = true;
-                                break;
-                            }
-                            else
-                                Ignored = false;
-                        }
-                        if (!Ignored)
-                        {
-                          
-                            List<Coordinate> pattern = new List<Coordinate>();
-                            FindOutlineSequence(pixel, bitmap, pattern);
-                            SL.Add(pattern);
-                        }
-                    }
-                }
-            }
-            return SL;
-        }
-
-        internal void FindOutlineSequence(Coordinate pixel, Bitmap bitmap, List<Coordinate> sequence)
-        {
-            bool Ignored = false;
-
-            foreach (Coordinate pixelToCompare in IgnoredPixels)
-            {
-                if (pixel.Compare(pixelToCompare))
-                {
-                    Ignored = true;
-                    break;
-                }
-            }
-            if (Ignored) return;
-            sequence.Add(pixel);
-            IgnoredPixels.Add(pixel);
-
-            Coordinate[] SurroundingPixels =
-            {
-                new Coordinate(pixel.X()-1, pixel.Y()),
-                new Coordinate(pixel.X()+1, pixel.Y()),
-                new Coordinate(pixel.X(), pixel.Y()-1),
-                new Coordinate(pixel.X(), pixel.Y()+1),
-                new Coordinate(pixel.X()-1, pixel.Y()-1),
-                new Coordinate(pixel.X()+1, pixel.Y()+1),
-                new Coordinate(pixel.X()-1, pixel.Y()+1),
-                new Coordinate(pixel.X()+1, pixel.Y()-1),
-            };
-            //check the surrounding pixels of a pixel
-            foreach (Coordinate newPixel in SurroundingPixels)
-            {
-                //if any of them is red
-                if (newPixel.X() >= 0 && newPixel.X() <= bitmap.Width && newPixel.Y() >= 0 && newPixel.Y() <= bitmap.Height)
-                    if (bitmap.GetPixel(newPixel.X(),newPixel.Y()) == Color.FromArgb(255,0,0))
-                {
-                    FindOutlineSequence(newPixel, bitmap, sequence);  
-                }
-            }
-           
-        }
-
-        internal List<List<Coordinate>>[] GenerateFillingSequences(Bitmap[,] patternMaps)
-        {
-            List<List<Coordinate>>[] FillingSequenceImageList = new List<List<Coordinate>>[(patternMaps.Length / 2)];
-            //loop over colour outlines
-            for (int i = 0; i < ((patternMaps.Length / 2)); i++)
-            {
-                List<List<Coordinate>> SequenceList = new List<List<Coordinate>>();
-                SequenceList = GenerateFillingSequenceList(patternMaps[i, 1]);
-                FillingSequenceImageList[i] = SequenceList;
-            }
-
-            return FillingSequenceImageList;
-        }
-
-        internal List<List<Coordinate>> GenerateFillingSequenceList(Bitmap bitmap)
-        {
-            IgnoredPixels = new List<Coordinate>();
-            int Width = bitmap.Width;
-            int Height = bitmap.Height;
-            List<List<Coordinate>> SL = new List<List<Coordinate>>();
-            for (int j = 0; j < Height; j++)
-            {
-                for (int i = 0; i < Width; i++)
-                {
-                    if (bitmap.GetPixel(i, j) == Color.FromArgb(0, 255, 0))
-                    {
-                        bool Ignored = false;
-                        Coordinate pixel = new Coordinate(i, j);
-                        foreach (Coordinate pixelToCompare in IgnoredPixels)
-                        {
-                            if (pixel.Compare(pixelToCompare))
-                            {
-                                Ignored = true;
-                                break;
-                            }
-                            else
-                                Ignored = false;
-                        }
-                        if (!Ignored)
-                        {
-
-                            List<Coordinate> pattern = new List<Coordinate>();
-                            FindFillingSequence(pixel, bitmap, pattern);
-                            SL.Add(pattern);
-                        }
-                    }
-                }
-            }
-            return SL;
-        }
-
-        internal void FindFillingSequence(Coordinate pixel, Bitmap bitmap, List<Coordinate> sequence)
-        {
-
-            bool Ignored = false;
-
-            foreach (Coordinate pixelToCompare in IgnoredPixels)
-            {
-                if (pixel.Compare(pixelToCompare))
-                {
-                    Ignored = true;
-                    break;
-                }
-            }
-            if (Ignored) return;
-            sequence.Add(pixel);
-            IgnoredPixels.Add(pixel);
-
-            Coordinate[] SurroundingPixels =
-            {
-                new Coordinate(pixel.X()+1, pixel.Y()),
-            };
-            //check the surrounding pixels of a pixel
-            foreach (Coordinate newPixel in SurroundingPixels)
-            {
-                //if any of them is red
-                if (pixel.X()-1 >= 0)
-                    if (bitmap.GetPixel(newPixel.X(), newPixel.Y()) == Color.FromArgb(0, 255, 0))
-                    {
-                        FindOutlineSequence(newPixel, bitmap, sequence);
-                    }
-            }
-        }
-
+ 
         internal void Handshake()
         {
             PlotterAvailable = false;
--- Plotter/Plotter.cs
+++ Plotter/Plotter.cs
@@ -9,8 +9,8 @@ using System.IO.Ports;
 
 public static class plotter
 {
+    static List<Coordinate> IgnoredPixels;
 
-	
 
     internal static Bitmap dither(Bitmap src1, int width, int height, List<Color> Compcol)
     {
@@ -122,6 +122,282 @@ public static class plotter
         return CMaps;
     }
 
+    internal static Bitmap[,] GeneratePatternMaps(Bitmap[] ColMaps, Form1 frm1)
+    {
+        //create a 2 dimensional array
+        Bitmap[,] Patterns = new Bitmap[ColMaps.Length - 1, 2];
+        //take colour image
+        FormLoadingcs frmldgn = new FormLoadingcs();
+        frm1.PatternProgressCount = -1;
+        frmldgn.Show();
+        int counter = 0;
+        for (int i = 0; i < ColMaps.Length - 1; i++)
+        {
+            counter++;
+            Bitmap bmp = ColMaps[i];
+            //create 2 bitmaps, one for the outline and the other for the filling
+            Bitmap Outline = new Bitmap(bmp.Width, bmp.Height, PixelFormat.Format24bppRgb);
+            Bitmap Filling = new Bitmap(bmp.Width, bmp.Height, PixelFormat.Format24bppRgb);
 
+            if (i < frm1.compcol.Count - 1)
+                frmldgn.Text = "Generating Outline - " + frm1.Coloursinv[frm1.compcol[i]] + " (" + (counter) + "/" + ((frm1.compcol.Count - 1) * 2) + ")";
+
+            //call an outlining method on the first bitmap, specify colour
+            Outline = FindOutline(Color.FromArgb(255, 0, 0), bmp, frmldgn, frm1);
+            //call a filling method on the second bitmap, specify colour
+            counter++;
+            if (i < frm1.compcol.Count - 1)
+                frmldgn.Text = "Generating Filling - " + frm1.Coloursinv[frm1.compcol[i]] + " (" + (counter) + "/" + ((frm1.compcol.Count - 1) * 2) + ")";
+
+            Filling = FindFilling(Color.FromArgb(0, 255, 0), Outline, bmp, frmldgn, frm1);
+            //add the 2 bitmaps to the Array
+            Patterns[i, 0] = Outline;
+            Patterns[i, 1] = Filling;
+        }
+        frmldgn.Close();
+        return Patterns;
+    }
+
+    internal static Bitmap FindOutline(Color colour, Bitmap MaptoCheck, FormLoadingcs frmldgn, Form1 frm1)
+    {
+        //create a list of coordinates to ignore
+        Bitmap linmap = new Bitmap(MaptoCheck.Width, MaptoCheck.Height, PixelFormat.Format24bppRgb);
+        //loop though colour map
+        for (int y = 0; y < MaptoCheck.Height; y++)
+        {
+            frm1.PatternProgressCount++;
+            for (int x = 0; x < MaptoCheck.Width; x++)
+            {
+                Coordinate pixel = new Coordinate(x, y);
+                //if the pixel is a 1 (black) then check pixel
+                if (MaptoCheck.GetPixel(x, y) == Color.FromArgb(0, 0, 0))
+                {
+                    CheckPixel(pixel, colour, MaptoCheck, linmap);
+                }
+            }
+            frmldgn.setProgress((int)(((float)frm1.PatternProgressCount / (float)MaptoCheck.Height * 100f) / (float)((frm1.compcol.Count - 1) * 2)));
+        }
+        return linmap;
+    }
+
+    internal static void CheckPixel(Coordinate pixel, Color colour, Bitmap MapTC, Bitmap MapTA)
+    {
+        Coordinate[] SurroundersDirect =
+       {
+                new Coordinate(pixel.X()-1,pixel.Y()),
+                new Coordinate(pixel.X()+1,pixel.Y()),
+                new Coordinate(pixel.X(),pixel.Y()-1),
+                new Coordinate(pixel.X(),pixel.Y()+1),
+            };
+        //check the 4 direct pixels surrounding the pixel(up down left right)
+        foreach (Coordinate coord in SurroundersDirect)
+        {
+
+            //if any of them is white then turn the pixel red and keep checking
+            if (coord.X() > 0 && coord.X() < MapTC.Width && coord.Y() > 0 && coord.Y() < MapTC.Height)
+            {
+                if (MapTC.GetPixel(coord.X(), coord.Y()) == Color.FromArgb(255, 255, 255))
+                {
+                    MapTA.SetPixel(pixel.X(), pixel.Y(), Color.FromArgb(255, 0, 0));
+                }
+            }
+        }
+    }
+
+    internal static Bitmap FindFilling(Color colour, Bitmap OutlineMap, Bitmap MapToFill, FormLoadingcs frmldgn, Form1 frm1)
+    {
+        Bitmap FillingMap = new Bitmap(OutlineMap.Width, OutlineMap.Height, PixelFormat.Format24bppRgb);
+        for (int y = 0; y < OutlineMap.Height; y++)
+        {
+            frm1.PatternProgressCount++;
+            for (int x = 0; x < OutlineMap.Width; x++)
+            {
+                if (MapToFill.GetPixel(x, y) == Color.FromArgb(0, 0, 0))
+                {
+                    if (OutlineMap.GetPixel(x, y) != Color.FromArgb(255, 0, 0))
+                    {
+                        FillingMap.SetPixel(x, y, colour);
+                    }
+                }
+            }
+            frmldgn.setProgress((int)(((float)frm1.PatternProgressCount / (float)MapToFill.Height * 100f) / (float)((frm1.compcol.Count - 1) * 2)));
+        }
+        return FillingMap;
+    }
+
+    internal static List<List<Coordinate>>[] GenerateOutlineSequences(Bitmap[,] patternMaps)
+    {
+        List<List<Coordinate>>[] OutlineSequenceImageList = new List<List<Coordinate>>[(patternMaps.Length / 2)];
+        //loop over colour outlines
+        for (int i = 0; i < ((patternMaps.Length / 2)); i++)
+        {
+            List<List<Coordinate>> SequenceList = new List<List<Coordinate>>();
+            SequenceList = GenerateOutlineSequenceList(patternMaps[i, 0]);
+            OutlineSequenceImageList[i] = SequenceList;
+        }
+
+        return OutlineSequenceImageList;
+    }
+
+    internal static List<List<Coordinate>> GenerateOutlineSequenceList(Bitmap bitmap)
+    {
+        IgnoredPixels = new List<Coordinate>();
+        int Width = bitmap.Width;
+        int Height = bitmap.Height;
+        List<List<Coordinate>> SL = new List<List<Coordinate>>();
+        for (int j = 0; j < Height; j++)
+        {
+            for (int i = 0; i < Width; i++)
+            {
+                if (bitmap.GetPixel(i, j) == Color.FromArgb(255, 0, 0))
+                {
+                    bool Ignored = false;
+                    Coordinate pixel = new Coordinate(i, j);
+                    foreach (Coordinate pixelToCompare in IgnoredPixels)
+                    {
+                        if (pixel.Compare(pixelToCompare))
+                        {
+                            Ignored = true;
+                            break;
+                        }
+                        else
+                            Ignored = false;
+                    }
+                    if (!Ignored)
+                    {
+
+                        List<Coordinate> pattern = new List<Coordinate>();
+                        FindOutlineSequence(pixel, bitmap, pattern);
+                        SL.Add(pattern);
+                    }
+                }
+            }
+        }
+        return SL;
+    }
+
+    internal static void FindOutlineSequence(Coordinate pixel, Bitmap bitmap, List<Coordinate> sequence)
+    {
+        bool Ignored = false;
+
+        foreach (Coordinate pixelToCompare in IgnoredPixels)
+        {
+            if (pixel.Compare(pixelToCompare))
+            {
+                Ignored = true;
+                break;
+            }
+        }
+        if (Ignored) return;
+        sequence.Add(pixel);
+        IgnoredPixels.Add(pixel);
+
+        Coordinate[] SurroundingPixels =
+        {
+                new Coordinate(pixel.X()-1, pixel.Y()),
+                new Coordinate(pixel.X()+1, pixel.Y()),
+                new Coordinate(pixel.X(), pixel.Y()-1),
+                new Coordinate(pixel.X(), pixel.Y()+1),
+                new Coordinate(pixel.X()-1, pixel.Y()-1),
+                new Coordinate(pixel.X()+1, pixel.Y()+1),
+                new Coordinate(pixel.X()-1, pixel.Y()+1),
+                new Coordinate(pixel.X()+1, pixel.Y()-1),
+            };
+        //check the surrounding pixels of a pixel
+        foreach (Coordinate newPixel in SurroundingPixels)
+        {
+            //if any of them is red
+            if (newPixel.X() >= 0 && newPixel.X() <= bitmap.Width && newPixel.Y() >= 0 && newPixel.Y() <= bitmap.Height)
+                if (bitmap.GetPixel(newPixel.X(), newPixel.Y()) == Color.FromArgb(255, 0, 0))
+                {
+                    FindOutlineSequence(newPixel, bitmap, sequence);
+                }
+        }
+
+    }
+
+    internal static List<List<Coordinate>>[] GenerateFillingSequences(Bitmap[,] patternMaps)
+    {
+        List<List<Coordinate>>[] FillingSequenceImageList = new List<List<Coordinate>>[(patternMaps.Length / 2)];
+        //loop over colour outlines
+        for (int i = 0; i < ((patternMaps.Length / 2)); i++)
+        {
+            List<List<Coordinate>> SequenceList = new List<List<Coordinate>>();
+            SequenceList = GenerateFillingSequenceList(patternMaps[i, 1]);
+            FillingSequenceImageList[i] = SequenceList;
+        }
+
+        return FillingSequenceImageList;
+    }
+
+    internal static List<List<Coordinate>> GenerateFillingSequenceList(Bitmap bitmap)
+    {
+        IgnoredPixels = new List<Coordinate>();
+        int Width = bitmap.Width;
+        int Height = bitmap.Height;
+        List<List<Coordinate>> SL = new List<List<Coordinate>>();
+        for (int j = 0; j < Height; j++)
+        {
+            for (int i = 0; i < Width; i++)
+            {
+                if (bitmap.GetPixel(i, j) == Color.FromArgb(0, 255, 0))
+                {
+                    bool Ignored = false;
+                    Coordinate pixel = new Coordinate(i, j);
+                    foreach (Coordinate pixelToCompare in IgnoredPixels)
+                    {
+                        if (pixel.Compare(pixelToCompare))
+                        {
+                            Ignored = true;
+                            break;
+                        }
+                        else
+                            Ignored = false;
+                    }
+                    if (!Ignored)
+                    {
+
+                        List<Coordinate> pattern = new List<Coordinate>();
+                        FindFillingSequence(pixel, bitmap, pattern);
+                        SL.Add(pattern);
+                    }
+                }
+            }
+        }
+        return SL;
+    }
+
+    internal static void FindFillingSequence(Coordinate pixel, Bitmap bitmap, List<Coordinate> sequence)
+    {
+
+        bool Ignored = false;
+
+        foreach (Coordinate pixelToCompare in IgnoredPixels)
+        {
+            if (pixel.Compare(pixelToCompare))
+            {
+                Ignored = true;
+                break;
+            }
+        }
+        if (Ignored) return;
+        sequence.Add(pixel);
+        IgnoredPixels.Add(pixel);
+
+        Coordinate[] SurroundingPixels =
+        {
+                new Coordinate(pixel.X()+1, pixel.Y()),
+            };
+        //check the surrounding pixels of a pixel
+        foreach (Coordinate newPixel in SurroundingPixels)
+        {
+            //if any of them is red
+            if (pixel.X() - 1 >= 0 && pixel.X() + 1 <= bitmap.Width)
+                if (bitmap.GetPixel(newPixel.X(), newPixel.Y()) == Color.FromArgb(0, 255, 0))
+                {
+                    FindOutlineSequence(newPixel, bitmap, sequence);
+                }
+        }
+    }
 
 }
